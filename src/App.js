945b8f0d9bcb2d422e6caa8463e1e13f15872dd6
Yogesh Kumar <ykapr17@gmail.com>
2021-09-26 21:07:23 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react'
+import './App.css'
+import Form from './components/Form'
+import Todolist from './components/Todolist';
+
+function App() {
+
+   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+
+  useEffect(()=> {
+    filterHandler()
+  }, [todos, status]) 
+
+  const filterHandler = () => {
+    switch(status){
+      case 'completed' : 
+      setFilteredTodos(todos.filter(todo => todo.completed ===true))
+      break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed ===false))
+        break;
+        default:
+          setFilteredTodos(todos) ;
+          break;
+    }
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Yogesh's Todo List</h1>
+        
+      </header>
+      <Form inputText = {inputText}
+       todos = {todos} 
+        setTodos = {setTodos} 
+        setInputText = {setInputText}
+        setStatus = {setStatus}
+       />
+      <Todolist setTodos = {setTodos} todos = {todos} 
+        filteredTodos = {filteredTodos}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todolist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Todo from './Todo'
+
+const Todolist = ({todos, setTodos, filteredTodos}) => {
+    return (
+        <div className = "todo-container">
+            <ul className = "todo-list">
+       {filteredTodos.map((todo)=>(
+           <Todo key = {todo.id}
+            text = {todo.text}
+            todos ={todos}
+            setTodos ={setTodos}
+            todo = {todo}/>
+       ))}
+        </ul>
+        </div>
+    )
+}
+
+export default Todolist;
